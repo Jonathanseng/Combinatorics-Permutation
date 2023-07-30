617ba467b7a15d7c8fed16df030fbe49b1e0fd88
Seng Keat <corodostudio@gmail.com>
2023-07-30 09:24:55 +0700

    Inclusion exclusion principal

---
+++ 20. Inclusion exclusion principle.py
@@ -0,0 +1,28 @@
+def inclusion_exclusion(sets):
+  """
+  Returns the cardinality of the union of the given sets.
+
+  Args:
+    sets: A list of sets.
+
+  Returns:
+    The cardinality of the union of the given sets.
+  """
+
+  result = 0
+  for set in sets:
+    result += len(set)
+  for i in range(len(sets)):
+    for j in range(i + 1, len(sets)):
+      if sets[i] & sets[j]:
+        result -= len(sets[i] & sets[j])
+  return result
+
+
+def main():
+  sets = [[1, 2, 3], [2, 3, 4], [3, 4, 5]]
+  print(inclusion_exclusion(sets))
+
+
+if __name__ == '__main__':
+  main()
